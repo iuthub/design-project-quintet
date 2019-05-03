f6fc420defe263f9a356d37658dbf37d3aaaa03e
Jasur_Salimov <jasur.salimov.96@gmail.com>
2019-05-03 07:50:03 +0500

    Commitment 2(night without sleep)

---
+++ resources/views/Layout/layout.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+        <title>LI-NING Uzbekistan</title>
+
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('/css/app.css')}}">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+       {{--  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> --}}
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Styles -->
+       
+    </head>
+    <body>
+<hr class="line">
+
+@include('includes.nav')
+
+<hr class="line">
+
+@yield('slides')
+
+{{-- content --}}
+
+
+            <div class="content">
+                @yield('content')
+            </div>
+       
+@yield('extra')
+{{-- content end --}}
+
+@include('includes.links')
+       
+@include('includes.footer')
+       
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../../../dist/js/bootstrap.min.js"></script>
+    <script src="../../../../assets/js/vendor/holder.min.js"></script>
+    <script>
+      Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+      });
+    </script>
+    </body>
+</html>
---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,36 @@
+
+{{-- Navbar --}}
+       <nav class="navbar site-header navbar-light navbar-expand-lg  justify-content-center sticky-top">
+      <div class="container">
+        <a class="navbar-brand" href="{{url('')}}"><img class="logo" src="{{URL::asset('/image/li-ning-black_large.png')}}"></a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars" aria-controls="navbars" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+ 
+        <div class="collapse navbar-collapse" id="navbars">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="{{url('')}}">Главная <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#"> Интернет магазин <span class="sr-only">(current)</span></a>
+            </li>
+           
+            <li class="nav-item active">
+              <a class="nav-link" href="#}"> О нас <span class="sr-only">(current)</span></a>
+            </li>
+
+             <li class="nav-item active">
+              <a class="nav-link" href="#"> Блог <span class="sr-only">(current)</span></a>
+            </li>
+
+          
+          </ul>
+            <a class="nav-link ml-auto" href="#"><i class="fa fa-shopping-cart navb"></i></a>
+        </div>
+      </div>
+    </nav>
+
+{{-- Navbar end --}}
